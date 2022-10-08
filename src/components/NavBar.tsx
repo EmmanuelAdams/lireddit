80a25ff5f8cc39d9247f35c8cc2fbf565fd87447
unknown <oluwaseunadams17@gmail.com>
2022-10-08 20:00:21 +0100

    deploy_web

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import {
+  useLogoutMutation,
+  useMeQuery,
+} from '../generated/graphql';
+import { isServer } from '../utils/isServer';
+import { useRouter } from 'next/router';
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const router = useRouter();
+  const [{ fetching: logoutFetching }, logout] =
+    useLogoutMutation();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(),
+  });
+  let body = null;
+
+  // data is loading
+  if (fetching || isServer()) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>register</Link>
+        </NextLink>
+      </>
+    );
+  }
+  // user is logged in
+  else {
+    body = (
+      <Flex align="center">
+        <NextLink href="/create-post">
+          <Button as={Link} mr={4}>
+            create post
+          </Button>
+        </NextLink>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button
+          // typedefs UseMutationState, make variable
+          // optional pending update
+          onClick={() => {
+            logout();
+            router.reload();
+          }}
+          isLoading={logoutFetching}
+          variant="link"
+          colorScheme="black">
+          logout
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex
+      zIndex={1}
+      position="sticky"
+      top={0}
+      bg="teal"
+      p={4}>
+      <Flex flex={1} m="auto" align="center" maxW={800}>
+        <NextLink href="/">
+          <Link>
+            <Heading>LiReddit</Heading>
+          </Link>
+        </NextLink>
+        <Box ml={'auto'}>{body}</Box>
+      </Flex>
+    </Flex>
+  );
+};
---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+import theme from '../theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
